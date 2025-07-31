345d842fddc6019151206004885fe401178438e4
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-07-31 10:28:44 -0300

    chore: commit initial

---
+++ src/main/java/com/ewerton/hexagonal_client/HexagonalClientApplication.java
@@ -0,0 +1,13 @@
+package com.ewerton.hexagonal_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewerton/hexagonal_client/HexagonalClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewerton.hexagonal_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
