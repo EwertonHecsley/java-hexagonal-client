f3b7b43b16d7733438e5f5f128bd58c32a9db074
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-07-31 10:53:03 -0300

    feat: create model Client

---
+++ src/main/java/com/ewerton/hexagonal_client/core/domain/model/Client.java
@@ -0,0 +1,43 @@
+package com.ewerton.hexagonal_client.core.domain.model;
+
+public class Client {
+    private String name;
+    private String email;
+
+    public Client(String name, String email) {
+        this.updateName(name);
+        this.updateEmail(email);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void updateName(String name) {
+        if (name.isBlank() || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be blank or empty");
+        }
+
+        this.name = name;
+    }
+
+    public void updateEmail(String email) {
+        if (email.isBlank() || email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be blank or empty");
+        }
+
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + this.name + '\'' +
+                ", email='" + this.email + '\'' +
+                '}';
+    }
+}
