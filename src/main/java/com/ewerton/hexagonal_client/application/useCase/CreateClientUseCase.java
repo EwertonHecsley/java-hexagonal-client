6a1f274f39436cd34ca329d77bbed8f4853d3f3e
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-07-31 11:16:43 -0300

    feat: create useCase

---
+++ src/main/java/com/ewerton/hexagonal_client/application/useCase/CreateClientUseCase.java
@@ -0,0 +1,21 @@
+package com.ewerton.hexagonal_client.application.useCase;
+
+import com.ewerton.hexagonal_client.core.domain.model.Client;
+import com.ewerton.hexagonal_client.core.domain.port.out.ClientRepositoryPort;
+
+public class CreateClientUseCase {
+    private final ClientRepositoryPort clientReposiory;
+
+    public CreateClientUseCase(ClientRepositoryPort clientRepository) {
+        this.clientReposiory = clientRepository;
+    }
+
+    public Client execute(String name, String email) {
+        try {
+            Client client = new Client(name, email);
+            return clientReposiory.create(client);
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating client: " + e.getMessage(), e);
+        }
+    }
+}
